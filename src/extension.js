4db3484f9118b479e80daa5e324c2a2ada0c158e
Jack Whelan <61770531+jw176@users.noreply.github.com>
2025-09-14 11:54:33 +1000

    Add more dynamic context values

--- src/extension.js
+++ src/extension.js
@@ -3,40 +3,42 @@ const fs = require('fs');
 const vscode = require('vscode');
 const Handlebars = require('handlebars');
 
-async function getCurrentEditorGitStatus() {
+async function getSelectionContextData() {
 
 	const gitExtension = vscode.extensions.getExtension('vscode.git').exports;
 	const api = gitExtension.getAPI(1);
 
-	
+	const selection = vscode.window.activeTextEditor.selection;
+	const selectedText = vscode.window.activeTextEditor.document.getText(selection);
+
 	const editor = vscode.window.activeTextEditor;
 	if (!editor) return null;
 	const uri = editor.document.uri;
 	const repo = api.repositories.find(r => uri.fsPath.startsWith(r.rootUri.fsPath));
-	const head = repo.state.HEAD;
 
-	// Get the branch and commit 
-	const { commit, name: branch } = head;
+	const currentBranch = repo.state.HEAD.name
+	const branchDetails = await repo.getBranch(currentBranch);
 
-	// Get head of any other branch
-	const mainBranch = 'master'
-	const branchDetails = await repo.getBranch(mainBranch);
-
-	// Get last merge commit
-	const lastMergeCommit = await repo.getMergeBase(branch, mainBranch);
-
-	const status = await repo.status();
-
-	console.log({ branch, status, branchDetails, commit, lastMergeCommit, needsSync: lastMergeCommit !== commit });
+	const changes = repo.state.workingTreeChanges;
+    const staged = repo.state.indexChanges;
+    const merge = repo.state.mergeChanges;
+    const untrackedCount = changes.filter(c => c.status === 7).length;
 
 	return {
-		file: uri,
-		root: repo.rootUri.fsPath,
-		repoName: path.basename(repo.rootUri.fsPath),
-		HEAD: repo.state.HEAD ? repo.state.HEAD.name || repo.state.HEAD.commit : null,
-		ahead: branchDetails.ahead,
-		behind: branchDetails.behind
-	};
+		repo: path.basename(repo.rootUri.fsPath),
+		branch: repo.state.HEAD.name,
+		commit: repo.state.HEAD.commit.slice(0, 7),
+		status: `${branchDetails.ahead}, ${branchDetails.behind}`,
+		stagedCount: staged.length,
+        unstagedCount: changes.length,
+		untrackedCount,
+        mergeInProgress: merge.length > 0,
+		file: path.relative(repo.rootUri.fsPath, uri.fsPath),
+		start: selection.start.line,
+		end: selection.end.line,
+		language: editor.document.languageId,
+		code: selectedText
+	}
 }
 
 /**
@@ -47,34 +49,17 @@ function activate(context) {
 	const templateSrc = fs.readFileSync(templatePath, 'utf-8');
 	const template = Handlebars.compile(templateSrc, { noEscape: true });
 
-	let myStatusBarItem;
 	const myCommandId = 'starpost-select';
 	context.subscriptions.push(vscode.commands.registerCommand(myCommandId, async () => {
-		const selection = vscode.window.activeTextEditor.selection;
-		const selectedText = vscode.window.activeTextEditor.document.getText(selection);
-
-		const gitData = await getCurrentEditorGitStatus();
-		const data = {
-			repo: gitData.repoName,
-			branch: gitData.HEAD,
-			commit: gitData.HEAD,
-			status: `${gitData.ahead}, ${gitData.behind}`,
-			file: gitData.file,
-			start: selection.start.line,
-			end: selection.end.line,
-			language: "js",
-			code: selectedText
-		}
-
+		const data = await getSelectionContextData();
 		const fancyStr = template(data);
-
 		await vscode.env.clipboard.writeText(fancyStr);
 		vscode.window.showInformationMessage(`Copied selection!`);
 	}));
-	myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 1000);
+
+	let myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 1000);
 	myStatusBarItem.command = myCommandId;
 	context.subscriptions.push(myStatusBarItem);
-
 	context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(updateStatusBarItem));
 	context.subscriptions.push(vscode.window.onDidChangeTextEditorSelection(updateStatusBarItem));
 
